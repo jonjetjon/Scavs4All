9210bf38f7337ddc3eebc7ffee9a979239c8e032
jonjetjon <jonjauch@live.com>
2024-09-30 10:24:21 -0600

    changed config files to make it work with spt server after a test run

---
+++ user/mods/zzzScavs4All/src/Scavs4All.ts
@@ -0,0 +1,76 @@
+import type { DependencyContainer } from "tsyringe"
+import type { ILogger } from "@spt-aki/models/spt/utils/ILogger"
+import type { IPostDBLoadMod } from "@spt-aki/models/external/IPostDBLoadMod"
+import type { DatabaseServer } from "@spt-aki/servers/DatabaseServer"
+
+class Scavs4All implements IPostDBLoadMod
+{
+  private container: DependencyContainer
+  private config = require("../config/config.json")
+  private logger :ILogger
+  private loggerBuffer :string[] = []
+  private replacePmc = false
+  private debug = false
+
+  public postDBLoad(container :DependencyContainer):void
+  {
+    this.container = container
+    this.logger = this.container.resolve<ILogger>("WinstonLogger")
+    const quests = this.container.resolve<DatabaseServer>("DatabaseServer").getTables().templates.quests
+
+    //go through each option in the config.json and handle known ones
+    for (let eachOption in this.config)
+    {
+      if (this.config[eachOption] != false)
+      {
+        switch (eachOption)
+        {
+          case 'debug':
+            this.debug = true
+            break
+
+          case 'ReplacePMCWithAll':
+            this.replacePmc = true
+            break
+        }
+      }
+    }
+    this.changeTargets(quests)
+    
+  }
+
+  private changeTargets(quests: any):void
+  {
+    //iterate through every quest in quests.json
+    for(let eachQuest in quests)
+        {
+            const currentQuest = quests[eachQuest]
+            //iterate through all the conditions of the current quest
+            for(let eachCondition in currentQuest.conditions.AvailableForFinish)
+            {
+                const currentCondition = currentQuest.conditions.AvailableForFinish[eachCondition]
+                //check if the current condition is a countercreator
+                if(currentCondition.conditionType === 'CounterCreator')
+                {
+                    //if it is a countercreator iterate through the subconditions
+                    for(let eachSubCondition in currentCondition.counter.conditions )
+                    {
+                      const specificCondition = currentCondition.counter.conditions[eachSubCondition]
+                      //check if the current subcondition is a kill condition and if it requires scav kills
+                      if (specificCondition.conditionType === 'Kills' && specificCondition.target === 'Savage')
+                      {  
+                        if(this.debug == true)
+                          {
+                            this.logger.info("Found a scav kill quest condition in quest name: " + currentQuest.QuestName + " replacing kill condition with any" )
+                          }
+                        //if it does replace the condition with any target
+                        quests[eachQuest].conditions.AvailableForFinish[eachCondition].counter.conditions[eachSubCondition].target = 'any';
+                      }
+                    }
+                }
+            }
+        }
+  }
+}
+
+module.exports = {mod: new Scavs4All()}
